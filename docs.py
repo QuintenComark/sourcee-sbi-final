af02689b65159b1dc674a392f8012c346b2f6a1f
Quinten Ceuppens <quintenceuppens@Quintens-MacBook-Pro.local>
2024-08-12 15:11:21 +0200

    added docs tabs

--- docs.py
+++ docs.py
@@ -11,3 +11,13 @@ logo = 'https://nveil.ai/wp-content/uploads/2024/07/sourcee-logo-v2.png'
 
 
 st.logo(logo)
+
+
+prompts, tipstricks , faq = st.tabs(["Example Prompts", "Tips & Tricks", "FAQ"])
+
+with prompts:
+    st.write("Example Prompts")
+with tipstricks:
+    st.write("Tips & Tricks")
+with faq:
+    st.write("Freqently Asked Questions")
