1df0d5cc7029df6785dfe43193179b08835ff1cc
Quinten Ceuppens <quintenceuppens@Quintens-MacBook-Pro.local>
2024-08-12 15:27:48 +0200

    added some prompts

--- docs.py
+++ docs.py
@@ -19,14 +19,14 @@ with prompts:
     st.title("Example Prompts")
     st.write("Here are some example prompts to get you started:")
     #create an expander for each prompt
-    with st.expander("Prompt 1"):
-        st.write("Prompt 1 text")
-    with st.expander("Prompt 2"):
-        st.write("Prompt 2 text")
-    with st.expander("Prompt 3"):
-        st.write("Prompt 3 text")
-   
-
+    with st.expander("Tell me about..."):
+        st.write("Tell me about 'name of teammember'. For example: what are their strengths and weaknesses, what is their experience, what are their skills, etc.")
+    with st.expander("Help me find..."):
+        st.write("Help me find a 'name of role'. The candidate should have experience in 'skill', 'skill', and 'skill'. A minimum of 'years' years of experience is required. Only tell me his name, contact info and a brief summary of his experience.")
+    with st.expander("Help me write..."):
+        st.write("Help me write an engaging text to introduce 'name of teammember'. The text should be professional and engaging, and should highlight their strengths and experience.")
+with tipstricks:
+    st.title("Tips & Tricks")
 with faq:
     st.title("Freqently Asked Questions")
     st.write("Here are some frequently asked questions:")
