b37e6b90d61e8c8eaefa5f2b2d78f59a35b46508
Quinten Ceuppens <quintenceuppens@Quintens-MacBook-Pro.local>
2024-08-12 15:02:08 +0200

    fix error 2

---
+++ docs.py
@@ -0,0 +1,13 @@
+import os
+import base64
+import re
+import json
+import requests
+import streamlit as st
+import openai
+import time
+
+logo = 'https://nveil.ai/wp-content/uploads/2024/07/sourcee-logo-v2.png'
+
+
+st.logo(logo)
